dc4eb03300067bebfbd722c692f5d1b1c800a048
Marina Paskal <paskalm@rogers.com>
2024-08-07 14:25:08 -0400

    Enable sign in with Google

--- public/index.js
+++ public/index.js
@@ -13,10 +13,11 @@ const $$ = Dom7;
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     app.tab.show("#tab2", true);
-    console.log(user);
+    console.log("User signed in: ", user.displayName);
+    // You can update UI elements here to show user info
   } else {
     app.tab.show("#tab1", true);
-    console.log("logged out");
+    console.log("User signed out");
   }
 });
 
@@ -72,3 +73,38 @@ $$("#logout").on("click", () => {
       // An error happened.
     });
 });
+
+// Google Sign-In function
+function googleSignIn() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // You might want to close the login screen here
+      app.loginScreen.close(".loginYes", true);
+      app.loginScreen.close(".signupYes", true);
+    })
+    .catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      if (errorCode === "auth/popup-closed-by-user") {
+        console.log("Sign-in popup closed by user.");
+        // You might want to show a message to the user
+        $$("#signInError").html(
+          "Sign-in cancelled. Please try again if you want to sign in."
+        );
+      } else {
+        console.error("Google sign in error: ", errorCode, errorMessage);
+        $$("#signInError").html(errorCode + " error " + errorMessage);
+      }
+    });
+}
+
+// Add event listeners for Google Sign-In buttons
+$$(".googleSignIn").on("click", googleSignIn);
